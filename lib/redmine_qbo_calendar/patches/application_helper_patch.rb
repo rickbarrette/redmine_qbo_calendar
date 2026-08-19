edd75af01b655a52c23c7ddba0d0f4d0bd630c47
Rick Barrette <rickbarrette@gmail.com>
2026-08-19 08:20:02 -0400

    update the favicon without java script

--- lib/redmine_qbo_calendar/patches/application_helper_patch.rb
+++ lib/redmine_qbo_calendar/patches/application_helper_patch.rb
@@ -9,16 +9,31 @@
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 module RedmineQboCalendar
-  def self.setup
-    # Patches
-    Customer.prepend(RedmineQboCalendar::Patches::CustomerPatch)
-    User.prepend(RedmineQboCalendar::Patches::UserPatch)
-    CalendarsController.prepend(RedmineQboCalendar::Patches::CalendarsControllerPatch)
-    ApplicationHelper.prepend(RedmineQboCalendar::Patches::ApplicationHelperPatch)
+  module Patches
+    module ApplicationHelperPatch
 
-    # Hooks
-    RedmineQboCalendar::Hooks::ViewHookListener
-    RedmineQboCalendar::Hooks::CustomerActionsHookListener
-    RedmineQboCalendar::Hooks::CustomerShowHookListener
+      def favicon
+        if calendar_or_appointments_view?
+          tag.link(rel: 'icon', type: 'image/png', href: favicon_path)
+        else
+          # Force an explicit type tag so the browser replaces the PNG on non-calendar pages
+          tag.link(rel: 'icon', type: 'image/x-icon', href: favicon_path)
+        end
+      end
+
+      def favicon_path
+        if calendar_or_appointments_view?
+          image_path("plugin_assets/redmine_qbo_calendar/favicons/#{Date.current.day}.png")
+        else
+          super
+        end
+      end
+
+      private
+
+      def calendar_or_appointments_view?
+        controller_name == 'calendars' || controller_name == 'customer_appointments'
+      end
+    end
   end
 end
