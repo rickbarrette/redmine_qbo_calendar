e3b2d33f8ef3f9ba69a90243ed5eb6a87e3aa450
Rick Barrette <rickbarrette@gmail.com>
2026-08-25 14:24:30 -0400

    Added session management for calendar month/year

--- lib/redmine_qbo_calendar/patches/calendars_controller_patch.rb
+++ lib/redmine_qbo_calendar/patches/calendars_controller_patch.rb
@@ -1,12 +1,12 @@
-#The MIT License (MIT)
+# The MIT License (MIT)
 #
-#Copyright (c) 2026 rick barrette
+# Copyright (c) 2026 rick barrette
 #
-#Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
-#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 module RedmineQboCalendar
   module Patches
@@ -14,6 +14,17 @@ module RedmineQboCalendar
 
       def show
         log "show"
+
+        # 1. Update or retrieve saved month/year from session before super runs
+        if params[:year].present? && params[:month].present?
+          session[:qbo_calendar_year]  = params[:year].to_i
+          session[:qbo_calendar_month] = params[:month].to_i
+        elsif session[:qbo_calendar_year].present? && session[:qbo_calendar_month].present?
+          params[:year]  ||= session[:qbo_calendar_year]
+          params[:month] ||= session[:qbo_calendar_month]
+        end
+
+        # 2. Call Redmine core CalendarsController#show
         super
 
         return unless @calendar
